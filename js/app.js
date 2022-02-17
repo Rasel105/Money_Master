b16f962b7e9c46a56cdf6ffa564a1d7bb863a866
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-02-17 19:11:19 +0600

    change complete

--- js/app.js
+++ js/app.js
@@ -1,45 +1,45 @@
-function expenseCalculation() {
+// main function start here 
+
+function totalSalary(){
+      // finding the salary input value 
     const salaryInput = document.getElementById("salary-input");
     const salaryInputValue = parseFloat(salaryInput.value);
+    return salaryInputValue
+}
+
+function expenseCalculation() {
+    // receiving the total salary value
+    const salaryInputValue = totalSalary()
+    
+    // 1st error handling
+
+    if(salaryInputValue > 0 && !isNaN(salaryInputValue)){
+
+        // fining the all expense part and setting the value property 
     const foodCost = document.getElementById("food-cost");
     const foodCostValue = parseFloat(foodCost.value);
+    
     const rentInput = document.getElementById("rent-cost");
     const rentInputValue = parseFloat(rentInput.value);
+ 
     const clothesInput = document.getElementById("clothes-cost");
     const clothesInputValue = parseFloat(clothesInput.value);
-
+   
     // expense calculation 
     const totalExpense = foodCostValue + rentInputValue + clothesInputValue
-    const finalExpense = salaryInputValue  - totalExpense
     
-    // total calculation 
-    document.getElementById("total-expense").innerText = totalExpense;
-    document.getElementById('balance').innerText = finalExpense;
-    salaryInput.value = ''
-    return finalExpense
-
+    if(totalExpense > salaryInputValue){
+        document.getElementById('fail3').style.display = 'block'
+    }
+    else{
+        document.getElementById('total-expense').innerText = totalExpense
+        const availableBalance =  document.getElementById('balance').innerText = salaryInputValue - totalExpense; 
+        return availableBalance;
+        }
+           }
+   else{
+        //    showing err to the UI 
+       document.getElementById('fail').style.display = 'block'
+   }
+  
 }
-
-function percentageCalculation()
-{
-    
-}
-
-
-document.getElementById('savings-btn').addEventListener('click', function () {
-    // receiving the existing balance 
-    const totalAmount = expenseCalculation();
-    console.log
-    const savingInput = document.getElementById('savings-input');
-    const salaryInputValue = savingInput.value;
-
-    const savingsPercentage = (totalAmount * salaryInputValue) / 100;
-    document.getElementById('savings-show').innerText = savingsPercentage;
-
-    document.getElementById('remaining-amount').innerText = totalAmount - savingsPercentage;
-});
-
-// finding calculate button 
-document.getElementById("calculate-btn").addEventListener("click", function () {
-    expenseCalculation();
-});
